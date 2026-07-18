ffc1a48005cc96629e6ebaef6e524dc424124ed6
smcoyle <smcoyle@wisc.edu>
2026-07-18 09:26:35 -0500

    chore: Export streamline generators from flow module

--- cellstream/flow/__init__.py
+++ cellstream/flow/__init__.py
@@ -1,4 +1,4 @@
-from .analytic import phase_velocity
+from .analytic import phase_velocity, generate_streamlines, generate_instantaneous_streamlines
 from .piv import binned_piv_velocity
 
-__all__ = ["phase_velocity", "binned_piv_velocity"]
+__all__ = ["phase_velocity", "binned_piv_velocity", "generate_streamlines", "generate_instantaneous_streamlines"]
