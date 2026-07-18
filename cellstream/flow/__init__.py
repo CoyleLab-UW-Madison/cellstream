d5b9528ea89f9132dc94fdcfa9eb72213a912c6c
smcoyle <smcoyle@wisc.edu>
2026-07-18 13:24:02 -0500

    feat: Add high-level flow processing pipeline

--- cellstream/flow/__init__.py
+++ cellstream/flow/__init__.py
@@ -1,4 +1,5 @@
 from .analytic import phase_velocity, generate_streamlines, generate_instantaneous_streamlines, compute_ftle, generate_phase_colored_streamlines
 from .piv import binned_piv_velocity
+from .process import process_cell_flow, process_zarr_store_flow
 
-__all__ = ["phase_velocity", "binned_piv_velocity", "generate_streamlines", "generate_instantaneous_streamlines", "compute_ftle", "generate_phase_colored_streamlines"]
+__all__ = ["phase_velocity", "binned_piv_velocity", "generate_streamlines", "generate_instantaneous_streamlines", "compute_ftle", "generate_phase_colored_streamlines", "process_cell_flow", "process_zarr_store_flow"]
